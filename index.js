fd561500d5d4565eaaf9930a1ea77ab723b88dd9
Elton Linconl <eltonlinconl07@gmail.com>
2019-05-17 22:26:15 -0300

    Configurações para subir o servidor

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-/**
- * Arquivo para criação de um servidor local 
- * utilizado para executar o código de produção.
- *
- * @author Márcio Casale de Souza <contato@kazale.com>
- * @since 0.0.6
- */
+const express = require('express');
+const path = require('path');
+const nomeApp = process.env.npm_package_name;
+const app = express();
 
-var path = require('path');
-var express = require('express');
-var app = express();
+app.use(express.static(`${__dirname}/dist/pitaco`));
 
-app.set('port', (process.env.PORT || 5000));
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// redireciona todas as requições para o Angular 2 
-app.all('*', function(req, res) {
-  res.status(200).sendFile(
-  	path.join(__dirname, 'dist', 'index.html'));
+app.get('/*', (req, res) => {
+res.sendFile(path.join(`${__dirname}/dist/pitaco/index.html`));
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node executando na porta ', app.get('port'));
+app.listen(process.env.PORT || 8080, function(){
+  console.log('Escutando na porta ' + process.env.PORT || 8080)
 });
